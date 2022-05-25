444a298e07119386267ed9dadc7b4c2600a5f587
paylo <abdellah.bahsine@hotmail.com>
2022-05-25 10:52:03 +0100

    I forgot to add app.js and index.js files to git...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
 
 function App() {
   return (
     <div className="app">
-      
+      <header>
+        <Link to="/" className="logo">
+          Logo
+        </Link>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login" className="login-btn">
+            <button>Login</button>
+          </Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
